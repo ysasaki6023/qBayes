660ddd4fd1047e22dc536a860355e4c41bb6624c
ysasaki <ysasaki6023@gmail.com>
2017-04-27 01:08:07 +0900

    update

--- naiveBayes.py
+++ naiveBayes.py
@@ -10,18 +10,19 @@ import cPickle as pickle
 class NaiveBayes:
     """Multinomial Naive Bayes"""
     def __init__(self):
-        self.categories = set()     # カテゴリの集合
-        self.vocabularies = set()   # ボキャブラリの集合
-        self.wordcount = {}         # wordcount[cat][word] カテゴリでの単語の出現回数
-        self.catcount = {}          # catcount[cat] カテゴリの出現回数
+        #self.categories = set()     # カテゴリの集合
+        #self.vocabularies = set()   # ボキャブラリの集合
+        self.wordcount = None         # wordcount[cat][word] カテゴリでの単語の出現回数
+        self.catcount = None          # catcount[cat] カテゴリの出現回数
         self.denominator = {}       # denominator[cat] P(word|cat)の分母の値
         self.textData = {}
         self.cateData = {}
         self.cacheDir = "cache"
     
     def __str__(self):
-        total = sum(self.catcount.values())
-        return "documents: %d, vocabularies: %d, categories: %d" % (total, len(self.vocabularies), len(self.categories))
+        #total = sum(self.catcount.values())
+        #print self.categories
+        return "documents: %d, vocabularies: %d, categories: %d" % (len(self.data), len(self.vocabularies), len(self.categories))
 
     def loadTextFile(self,fpath,index_column,columns_to_use=None):
         cacheName = fpath+str(index_column)+str(columns_to_use)
@@ -91,26 +92,45 @@ class NaiveBayes:
             if not index in self.textData:
                 print "%s is not in self.textData. Skip"%index
                 continue
-            self.data.append([index,self.textData[index]])
+            self.data.append([self.cateData[index],self.textData[index]])
         
         # 文書集合からカテゴリを抽出して辞書を初期化
+        self.categories = set()
         for cat,d in self.data:
             self.categories.add(cat)
-        for cat in self.categories:
-            self.wordcount[cat] = defaultdict(int)
-            self.catcount[cat] = 0
-        # 文書集合からカテゴリと単語をカウント
+        self.categories = list(self.categories)
+
+        # 全単語数を計算
+        self.vocabularies = set()
         for cnt,catdog in enumerate(self.data):
-            if cnt%1000==0: print "train(): processing %d/%d"%(cnt,len(self.data))
-            cat,doc = catdog
-            self.catcount[cat] += 1
+            if cnt%1000==0: print "train(): counting words %d/%d"%(cnt,len(self.data))
+            _,doc = catdog
             for word in doc:
                 if wordFilter and (not word in wordFilter): continue
                 self.vocabularies.add(word)
-                self.wordcount[cat][word] += 1
+        self.vocabularies = list(self.vocabularies)
+        self.vocab_lookup = {w:i for i,w in enumerate(self.vocabularies)}
+
+        # vocabulary 配列を作成
+        self.np_words = np.zeros((len(self.data),len(self.vocabularies)),dtype=np.int32)
+        self.np_categ = np.zeros(len(self.categories),dtype=np.int32)
+
+        # 文書集合からカテゴリと単語をカウント
+        for cnt,catdog in enumerate(self.data):
+            if cnt%100==0: print "train(): processing %d/%d"%(cnt,len(self.data))
+            cat,doc = catdog
+            idx_cat = self.categories.index(cat)
+            self.np_categ[idx_cat] += 1
+            for word in doc:
+                if wordFilter and (not word in wordFilter): continue
+                idx_word = self.vocab_lookup[word]
+                self.np_words[idx_cat][idx_word] += 1
+
         # 単語の条件付き確率の分母の値をあらかじめ一括計算
+        self.denominator = np.zeros(len(self.categories),dtype=np.float32)
         for cat in self.categories:
-            self.denominator[cat] = sum(self.wordcount[cat].values()) + len(self.vocabularies)
+            idx_cat = self.categories.index(cat)
+            self.denominator[idx_cat] = np.sum(self.np_words[idx_cat]) + len(self.vocabularies)
     
     def classify(self, doc):
         """事後確率の対数 log(P(cat|doc)) がもっとも大きなカテゴリを返す"""
@@ -154,42 +174,43 @@ class NaiveBayes:
             score += math.log(self.wordProb(word, cat))  # log P(word|cat)
         return score
     
-    def wordInfo(self,fpath=None,topn=None):
+    def wordInfo(self,fpath=None,topn=None,minfreq=None):
         """単語の性質を出力"""
-        words = set()
-        for cat in self.categories:
-            for w in self.wordcount[cat]:
-                words.add(w)
-
-        wordNum = {}
-        wordEnt = {}
-        for cnt,w in enumerate(words):
-            if cnt%10000==0: print "wordInfo(): processing %d/%d"%(cnt,len(words))
-            wordNum[w] = {}
-            for cat in self.categories:
-                wordNum[w][cat] = self.wordcount[cat][w]
-            entropy = 0.
-            for cat in self.categories:
-                prob = float(wordNum[w][cat])/sum(wordNum[w].values())
-                if prob > 0.:
-                    entropy -= prob * math.log(prob)
-            wordEnt[w] = entropy
+        prob = self.np_words.astype(np.float32) / np.sum(self.np_words,axis=0)
+        entropy = - prob * np.ma.log(prob)
+        entropy = np.sum(entropy, axis=0)
 
         if fpath:
             f = open(fpath, 'w')
             writer = csv.writer(f, lineterminator='\n')
 
-        cnt = 1
-        for word,entropy in sorted(wordEnt.items(), key=lambda x:x[1]):
-            if topn and cnt>topn: break
+        mask = np.zeros(len(self.vocabularies),dtype=bool)
+        mask[:] = True
+        if minfreq: mask[ np.sum(self.np_words,axis=0) < minfreq ] = False
+
+        wcounts = self.np_words[:,mask]
+        wordidx = np.linspace(0,self.np_words.shape[1]-1,num=self.np_words.shape[1],dtype=np.int32)
+        entropy = entropy[mask]
+
+        idx_sorted = np.argsort(entropy)[::-1]
+        wcounts = wcounts[:,idx_sorted]
+        wordidx = wordidx[idx_sorted]
+        entropy = entropy[idx_sorted]
+
+        if topn:
+            wcounts = wcounts[:,:topn]
+            wordidx = wordidx[:topn]
+            entropy = entropy[:topn]
+
+        for i in range(len(wordidx)):
+            if i%1000==0: print "wordInfo(): dumping %d/%d"%(i,len(wordidx))
             if fpath:
-                line = [cnt,word.encode("utf-8")]
-                for c in self.categories: line.append(wordNum[word][c])
-                line.append(entropy)
+                line = [i,self.vocabularies[wordidx[i]].encode("utf-8")]
+                for j in range(len(self.categories)): line.append(wcounts[j,i])
+                line.append(entropy[i])
                 writer.writerow(line)
             else:
-                print "%3d  H(%20s) = %.3e"%(cnt,word,entropy)
-            cnt += 1
+                print "%8d  H(%20s) = %.3e"%(i,self.vocabularies[wordidx[i]],entropy[i])
 
         if fpath: f.close()
 
@@ -206,7 +227,7 @@ if __name__ == "__main__":
     print "...done"
     print nb
     nb.wordInfo(topn=10)
-    nb.wordInfo(fpath="word.csv")
+    nb.wordInfo(fpath="word.csv",minfreq=100)
     #print nb.categories
     print "log P(1|は, ため) =", nb.score([u"は",u"ため"], u"1")
     print "log P(2|は, ため) =", nb.score([u"は",u"ため"], u"2")
