2bf561cfdf3838c6b37b1a4a4c2a50b549046bbb
ysasaki <ysasaki6023@gmail.com>
2017-04-28 02:11:16 +0900

    added evaluation()

--- naiveBayes.py
+++ naiveBayes.py
@@ -28,6 +28,12 @@ class NaiveBayes:
         p["vocab_lookup"] = self.vocab_lookup
         p["np_words"]     = self.np_words
         p["np_categ"]     = self.np_categ
+        p["textFile_fpath"] = self.textFile_fpath
+        p["textFile_index_column"]  = self.textFile_index_column
+        p["textFile_columns_to_use"] = self.textFile_columns_to_use
+        p["cateFile_fpath"] = self.cateFile_fpath
+        p["cateFile_index_column"]  = self.cateFile_index_column
+        p["cateFile_column_to_use"] = self.cateFile_column_to_use
 
         with open(fpath,"wb") as f:
             pickle.dump(p,f,protocol=2)
@@ -41,11 +47,22 @@ class NaiveBayes:
         self.vocab_lookup = p["vocab_lookup"]
         self.np_words     = p["np_words"]
         self.np_categ     = p["np_categ"]
+        self.textFile_fpath         = p["textFile_fpath"]
+        self.textFile_index_column  = p["textFile_index_column"]
+        self.textFile_columns_to_use = p["textFile_columns_to_use"]
+        self.cateFile_fpath         = p["cateFile_fpath"]
+        self.cateFile_index_column  = p["cateFile_index_column"]
+        self.cateFile_column_to_use = p["cateFile_column_to_use"]
 
         return
 
     def loadTextFile(self,fpath,index_column,columns_to_use=None):
-        cacheName = fpath+str(index_column)+str(columns_to_use)
+
+        self.textFile_fpath          = fpath
+        self.textFile_index_column   = index_column
+        self.textFile_columns_to_use = columns_to_use
+
+        cacheName = fpath+index_column.encode("utf-8")+"".join([x.encode("utf-8") for x in columns_to_use])+"_text"
         cacheName = cacheName.replace("/","").replace(".","").replace("[","").replace("]","").replace(",","").replace("'","").replace(" ","")
         cacheFname = os.path.join(self.cacheDir,cacheName+".pickle")
         if not os.path.exists(self.cacheDir):
@@ -55,8 +72,8 @@ class NaiveBayes:
                 self.textData = pickle.load(f)
             return
 
-        self.textfile_index_column   = index_column
-        self.textfile_columns_to_use = columns_to_use
+        self.textFile_index_column   = index_column
+        self.textFile_columns_to_use = columns_to_use
         tagger = MeCab.Tagger("-Ochasen")
 
         print "loading text file:%s"%fpath
@@ -94,9 +111,23 @@ class NaiveBayes:
         return
 
     def loadCategoryFile(self,fpath,index_column,column_to_use):
+        self.cateFile_fpath          = fpath
+        self.cateFile_index_column   = index_column
+        self.cateFile_column_to_use  = column_to_use
+
         print "loading category file:%s, categoryColumn:%s"%(fpath,column_to_use)
         self.catefile_index_column   = index_column
         self.catefile_column_to_use = column_to_use
+
+        cacheName = fpath+index_column.encode("utf-8")+column_to_use.encode("utf-8")+"_cate"
+        cacheName = cacheName.replace("/","").replace(".","").replace("[","").replace("]","").replace(",","").replace("'","").replace(" ","")
+        cacheFname = os.path.join(self.cacheDir,cacheName+".pickle")
+        if not os.path.exists(self.cacheDir):
+            os.makedirs(self.cacheDir)
+        if os.path.exists(cacheFname):
+            with open(cacheFname,"rb") as f:
+                self.cateData = pickle.load(f)
+            return
 
         with open(fpath, 'r') as f:
             reader = csv.reader(f)
@@ -110,18 +141,26 @@ class NaiveBayes:
                 cate  = row[idx_to_use]
 
                 self.cateData[index] = cate
+
+        with open(cacheFname,"wb") as f:
+            pickle.dump(self.cateData,f,protocol=2)
+
         return
 
-    def train(self,minfreq=None,wordFilter=None):
-        """ナイーブベイズ分類器の訓練"""
-        print "train()"
+    def buildData(self,verbose=False):
         # データを作成
         self.data = []
         for index in self.cateData:
             if not index in self.textData:
-                print "%s is not in self.textData. Skip"%index
+                if verbose: print "%s is not in self.textData. Skip"%index
                 continue
             self.data.append([self.cateData[index],self.textData[index]])
+        return
+
+    def train(self,minfreq=None):
+        """ナイーブベイズ分類器の訓練"""
+        print "train()"
+        self.buildData(verbose=True)
         
         # 文書集合からカテゴリを抽出して辞書を初期化
         self.categories = set()
@@ -136,7 +175,6 @@ class NaiveBayes:
             if cnt%1000==0: print "..counting words %d/%d"%(cnt,len(self.data))
             _,doc = catdog
             for word in doc:
-                if wordFilter and (not word in wordFilter): continue
                 if not word in self.vocabcount: self.vocabcount[word] = 0
                 self.vocabcount[word] += 1
         origNum = len(self.vocabcount)
@@ -210,28 +248,81 @@ class NaiveBayes:
         if oneNumFlag: return val[0]
         else         : return val
 
+    def scoreDict(self,doc,wordFilter=None):
+        scoreCat = self.scoreList(doc,wordFilter)
+        scoreDict = {}
+        for i, cat in enumerate(self.categories):
+            scoreDict[cat] = scoreCat[i]
+        return scoreDict
 
-    def scoreDict(self,doc):
-        probCat = self.scoreList(doc)
+    def probDict(self,doc,wordFilter=None):
+        probCat = self.probList(doc,wordFilter)
         probDict = {}
         for i, cat in enumerate(self.categories):
             probDict[cat] = probCat[i]
         return probDict
 
-    def scoreList(self,doc):
+    def scoreList(self,doc,wordFilter=None):
+        scoreCat = []
+        for c in self.categories:
+            scoreCat.append(self.score(doc,c,wordFilter))
+        return scoreCat
+
+    def probList(self,doc,wordFilter=None):
         probCat = []
         for c in self.categories:
-            probCat.append(math.exp(self.score(doc,c)))
+            probCat.append(math.exp(self.score(doc,c,wordFilter)))
         total = sum(probCat)
         for i,c in enumerate(self.categories):
             probCat[i] /= total
         return probCat
     
-    def score(self, doc, cat):
+    def score(self, doc, cat, wordFilter=None):
         """文書が与えられたときのカテゴリの事後確率の対数 log(P(cat|doc)) を求める"""
+        doc = self.applyFilter(doc)
         score  = np.log(self.np_categ[self.categories.index(cat)])/np.sum(self.np_categ)  # log P(cat)
         score += np.sum( np.log(self.wordProb( doc, cat ) ) ) # sum log P(doc|cat)
         return score
+
+    def applyFilter(self, doc, wordFilter=None):
+        if not wordFilter: return doc
+        dd = []
+        for w in doc:
+            if w in wordFilter: dd.append(w)
+        return dd
+    
+    def evaluate(self, wordFilter=None, fpath=None):
+        print "evaluate()"
+        print "..load data"
+        if not self.textData: self.loadTextFile(self.textFile_fpath,self.textFile_index_column,self.textFile_columns_to_use)
+        if not self.cateData: self.loadCategoryFile(self.cateFile_fpath,self.cateFile_index_column,self.cateFile_column_to_use)
+        if not self.data    : self.buildData()
+
+        nCat = len(self.categories)
+        mat = np.zeros((nCat,nCat),dtype=np.float32)
+
+        print
+        for cnt, catdog in enumerate(self.data):
+            if cnt%1000==0: print "..%d"%cnt
+            t, x = catdog
+            sd = self.scoreDict(x,wordFilter)
+            y  = max(sd.items(), key=lambda x:x[1])[0]
+            mat[self.categories.index(t),self.categories.index(y)] += 1
+
+        print
+        print mat
+
+        if fpath:
+            with open(fpath,"w") as f:
+                w = csv.writer(f)
+                line = [""] + [self.categories[x] for x in range(nCat)]
+                w.writerow(line)
+                for k in range(nCat):
+                    line = [self.categories[k]] + [mat[k,j] for j in range(nCat)]
+                    w.writerow(line)
+
+        pass
+
     
     def wordInfo(self,fpath=None,topn=None,minfreq=None):
         """単語の性質を出力"""
@@ -242,6 +333,9 @@ class NaiveBayes:
         if fpath:
             f = open(fpath, 'w')
             writer = csv.writer(f, lineterminator='\n')
+            line = ["index","word","entropy"] # vocabularies -> need to sort, thus use wordidx
+            for j in range(len(self.categories)): line.append(self.categories[j]) # wcounts -> already sorted
+            writer.writerow(line)
 
         mask = np.zeros(len(self.vocabularies),dtype=bool)
         mask[:] = True
@@ -264,8 +358,8 @@ class NaiveBayes:
         for i in range(len(wordidx)):
             if fpath:
                 line = [i,self.vocabularies[wordidx[i]].encode("utf-8")] # vocabularies -> need to sort, thus use wordidx
-                for j in range(len(self.categories)): line.append(wcounts[j,i]) # wcounts -> already sorted
                 line.append(entropy[i]) # entropy -> already sorted
+                for j in range(len(self.categories)): line.append(wcounts[j,i]) # wcounts -> already sorted
                 writer.writerow(line)
             else:
                 print "%8d  H(%20s) = %.3e"%(i,self.vocabularies[wordidx[i]],entropy[i])
@@ -278,7 +372,6 @@ if __name__ == "__main__":
     nb.loadTextFile    (fpath="data/mergedEDINET.csv",index_column=u"code4",columns_to_use=[u'situation', u'issue', u'risk', u'contract', u'research',u'financial'])
     nb.loadCategoryFile(fpath="data/category.csv" ,index_column=u"銘柄コード",column_to_use=u"結論")
     print "...done"
-    #nb.train(wordFilter=[u"は",u"ため",u"ゴム",u"以来"])
     nb.train(minfreq=100)
     print nb
     nb.save("test.pickle")
@@ -287,15 +380,8 @@ if __name__ == "__main__":
     nb2 = NaiveBayes()
     nb2.load("test.pickle")
     print nb2
-    nb2.wordInfo(topn=10)
-    nb2.wordInfo(fpath="word.csv",minfreq=100)
-    #print nb.categories
-    print "log P(1|暖冬) =", nb2.score([u"暖冬"], u"1")
-    print "log P(X|暖冬) =", nb2.scoreDict([u"暖冬"])
-    #print "log P(2|は, ため) =", nb.score([u"は",u"ため"], u"2")
-    #print "log P(X|は, ため) =", nb.scoreDict([u"は",u"ため"])
-    #print
-    #print "log P(1|は, ため, ゴム) =", nb.score([u"は",u"ため",u"ゴム"], u"1")
-    #print "log P(2|は, ため, ゴム) =", nb.score([u"は",u"ため",u"ゴム"], u"2")
-    #print "log P(X|は, ため, ゴム) =", nb.scoreDict([u"は",u"ため",u"ゴム"])
-    #print "log P(X|は, ため, 以来, が) =", nb.scoreDict([u"は",u"ため",u"以来",u"が"])
+    nb2.wordInfo(topn=100)
+    nb2.wordInfo(fpath="word.csv",minfreq=200)
+    nb2.evaluate(fpath="mat.csv")
+    #print "log P(1|暖冬) =", nb2.score([u"暖冬"], u"1")
+    #print "log P(X|暖冬) =", nb2.scoreDict([u"暖冬"])
